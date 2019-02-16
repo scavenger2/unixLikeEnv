15e885124709679891c61fb3435b16a6e5e224d5
scavenger2 <liverkiller@gmail.com>
2019-02-16 17:34:08 +0800

    add VIM plugins installation

---
+++ installation/vim_plugins.sh
@@ -0,0 +1,10 @@
+#Plug 'vim-airline/vim-airline'
+git clone https://github.com/vim-airline/vim-airline ~/vim/plugged/vim-airline
+#Plug 'xml.vim'
+git clone https://github.com/othree/xml.vim ~/vim/plugged/xml.vim
+#Plug 'json.vim'
+git clone https://github.com/elzr/vim-json.git ~/vim/plugged/vim-json
+#Plug 'vim-javascript'
+git clone https://github.com/pangloss/vim-javascript.git ~/vim/plugged/vim-javascript
+#Plug 'vim-jsx'
+git clone https://github.com/mxw/vim-jsx.git ~/vim/plugged/vim-jsx
